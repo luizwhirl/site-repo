c32ab615fc3ac7a9aa9f5f9d20ab0569dfccb912
luizwhirl <leeshupativani@gmail.com>
2025-11-17 12:35:18 -0300

    index.html is back

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  // Caso vá fazer deploy em subpasta, ajuste a base. Ex: '/meu-repo/'
-  base: '/', 
-  server: {
-    port: 3000
-  }
-})
